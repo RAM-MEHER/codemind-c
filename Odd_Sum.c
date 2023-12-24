aef9dac9c2abd74adb2b9de42c7c5e19646d8642
RAM-MEHER <147472978+RAM-MEHER@users.noreply.github.com>
2023-12-24 20:14:22 +0530

    #include<stdio.h> int main(){     int n;     scanf("%i" , &n);     int i , ar[n] , sum = 0;     for(i = 0 ; i < n ; i++){         scanf("%i" , &ar[i]);         if(ar[i] % 2 != 0){             sum = sum + ar[i];         }     }     printf("%i",sum);     return 0; }

---
+++ Odd_Sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%i" , &n);
+    int i , ar[n] , sum = 0;
+    for(i = 0 ; i < n ; i++){
+        scanf("%i" , &ar[i]);
+        if(ar[i] % 2 != 0){
+            sum = sum + ar[i];
+        }
+    }
+    printf("%i",sum);
+    return 0;
+}
