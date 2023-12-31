f2f7528cc2db3a13d8a91dcbd49607b0d57b54b1
RAM-MEHER <147472978+RAM-MEHER@users.noreply.github.com>
2023-12-31 10:48:23 +0530

    #include<stdio.h> int main(){     int n , count = 0;     scanf("%i" , &n);     int ar[n] , i , avg , sum = 0;     for(i = 0 ; i < n ; i++){         scanf("%i" ,&ar[i]);         sum = sum + ar[i];     }     avg = sum / n;     for(i = 0 ; i < n ; i++){         if(ar[i] <= avg) count++;     }     printf("%i" , count);     return 0; }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n , count = 0;
+    scanf("%i" , &n);
+    int ar[n] , i , avg , sum = 0;
+    for(i = 0 ; i < n ; i++){
+        scanf("%i" ,&ar[i]);
+        sum = sum + ar[i];
+    }
+    avg = sum / n;
+    for(i = 0 ; i < n ; i++){
+        if(ar[i] <= avg) count++;
+    }
+    printf("%i" , count);
+    return 0;
+}
